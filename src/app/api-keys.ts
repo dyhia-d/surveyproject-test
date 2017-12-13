3f726e0c3bbb32d9660daae16066e04a8bfdf78f
dyhia-d <dyhiadib@gmail.com>
2017-12-13 19:10:31 +0100

    to delete later

---
+++ src/app/api-keys.ts
@@ -0,0 +1,7 @@
+export var masterFirebaseConfig = {
+      apiKey: "AIzaSyCdLcZKPGcw8Q3IlxWj2_AAyTcXc4cnBZs",
+      authDomain: "surveys-4c266.firebaseapp.com",
+      databaseURL: "https://surveys-4c266.firebaseio.com",
+      storageBucket: "",
+      messagingSenderId: "603800091800"
+  };
